f40f7130ec83b273cc740cd172499b8e903ffca3
Razak Nadeem <razaknadeem786@gmail.com>
2026-07-30 18:29:13 +0530

    Changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        System.out.println("Welcome to the java track");
+       System.out.println("I already know how to build programming logic");
+       
     }
 }
