abcc2db6f316214f3d0a39c532fab9d7a6da3daf
Razak Nadeem <razaknadeem786@gmail.com>
2026-08-05 16:03:06 +0530

    Taking input from user

--- track/M02/cc.java
+++ track/M02/cc.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class cc {
     public static void main (String[] args){
-        int num = 8;
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        num = sc.nextInt();
+
         if(num > 10){
             System.out.println("Number is greater than 10");
         }
