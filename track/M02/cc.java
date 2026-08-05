5dd19ef414666337f1cf7f58f86f4ae0e7382403
Razak Nadeem <razaknadeem786@gmail.com>
2026-08-05 15:56:46 +0530

    if else program

--- track/M02/cc.java
+++ track/M02/cc.java
@@ -1,8 +1,11 @@
 public class cc {
     public static void main (String[] args){
-        int num = 11;
+        int num = 8;
         if(num > 10){
             System.out.println("Number is greater than 10");
+        }
+        else{
+            System.out.println("Number is not greater than 10");
         }
     }
 }
