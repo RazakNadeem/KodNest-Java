e419289fb977cc3461223b6bccd4e421b5957bdf
Razak Nadeem <razaknadeem786@gmail.com>
2026-08-06 14:25:40 +0530

    Switch problem

---
+++ track/M02/T01/ST01/Main4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main4 {
+    public static void main(String[] args){
+        int num;
+        System.out.println("Enter a Number");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        switch(num){
+            case 1 : System.out.println("One");
+            break;
+            case 2 : System.out.println("Two");
+            break;
+            case 3 : System.out.println("Three");
+            break;
+            default : System.out.println("Invalid");
+
+        }
+
+    }
+    
+}
