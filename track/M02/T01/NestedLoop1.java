2975c0ffbacb27d0de64b7ce01a09617f103ba37
Razak Nadeem <razaknadeem786@gmail.com>
2026-08-07 15:56:04 +0530

    Taking Input

---
+++ track/M02/T01/NestedLoop1.java
@@ -0,0 +1,5 @@
+public class NestedLoop1{
+    public static void main(String[] args){
+        
+    }
+}
---
+++ track/M02/T01/ST01/TakingUserInput1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TakingUserInput1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter byte value : ");
+
+		byte a = scan.nextByte();
+
+		System.out.println(a);
+
+		System.out.println("Enter integer value : ");
+
+		int b = scan.nextInt();
+
+		System.out.println(b);
+
+	}
+
+}
