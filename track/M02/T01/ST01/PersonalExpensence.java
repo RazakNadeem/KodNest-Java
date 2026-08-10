1018a973d5f1f84ffa6cd092cad2db2e7e1add58
Razak Nadeem <razaknadeem786@gmail.com>
2026-08-10 11:06:31 +0530

    Personal Expensence Calculator

---
+++ track/M02/T01/ST01/PersonalExpensence.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PersonalExpensence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read income and expenses
+        // Calculate and display the budget details
+        double monthlyIncome = scanner.nextDouble();
+        double rentExpense = scanner.nextDouble();
+        double foodExpense = scanner.nextDouble();
+        double travelExpense = scanner.nextDouble();
+        double totalExpense = rentExpense+foodExpense+travelExpense;
+        double remaining = monthlyIncome - totalExpense;
+        String status;
+        System.out.println("Total expense: "+totalExpense);
+        System.out.println("Remaining: "+remaining);
+        if(remaining >=0){
+            System.out.println("Status: Within budget");
+        } else {
+            System.out.println("Status: Over budget");
+        }
+
+        scanner.close();
+    }
+}
+}
