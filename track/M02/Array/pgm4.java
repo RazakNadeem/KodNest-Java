72ae2c704c4bb3c8d2c0a30ee102c04d99767168
Razak Nadeem <razaknadeem786@gmail.com>
2026-08-17 15:39:47 +0530

    Advanced For loop

---
+++ track/M02/Array/pgm4.java
@@ -0,0 +1,9 @@
+
+public class pgm4 {
+    public static void main(String[] args) {
+        int a[] = {10,20,30,40,50};
+        for(int x :a){
+            System.out.println(x);
+        }
+    }
+}
